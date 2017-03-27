12d7f985fe528d45b468b8dc8e6ca8124ba1aea7
Andrew Wong <wongandrew97@gmail.com>
2017-03-27 00:47:40 -0400

    Update filters.py

--- filters.py
+++ filters.py
@@ -5,24 +5,38 @@ import os, sys
 
 SIZE = 500
 
+def resize_square(im):
+	# if  square, make 500x500
+	width, height = im.size
+	if width > height:
+		height = int((SIZE*1.0/width) * height)
+		width = SIZE
+	else: 
+		width = int((SIZE*1.0/height) * width)
+		height = SIZE
+
+	im = im.resize((width, height), Image.ANTIALIAS)
+
 def batch():
 	''' 
 	To run, script must be outside photos directory.
 	First command line argument: photos directory
 
-	Usage: python filters.py dirname
+	Usage: python filters.py dirname filter.png
+
+	where filter.png is the name of the filter and it lies within the photo directory
 	'''
 	
 	try:
 		foldername = sys.argv[1]
 	except IndexError:
-		print 'No directory specified. Usage: python filters.py dirname'
+		print 'No directory specified. Usage: python filters.py foldername filtername(default=filter.png)'
 		sys.exit(1)
 
 	try:
 		os.listdir(foldername)
 	except OSError, e:
-		print 'Not a directory: ' + foldername + '. Usage: python filters.py dirname'
+		print 'Not a directory: ' + foldername + '. Usage: python filters.py foldername filtername(default=filter.png)'
 		sys.exit(1)
 	
 	try:
@@ -30,27 +44,32 @@ def batch():
 	except OSError, e:
 		pass
 
-	fb_filter = Image.open(foldername + "/filter.png")
+	try:
+		filter_path = sys.argv[2]
+	except IndexError:
+		filter_path = 'filter.png'
+
+	fb_filter = Image.open(foldername + '/' + filter_path)
 	for file in os.listdir(foldername):
 		# if ends with a valid extension
 		if file.endswith('.png') or file.endswith('.jpeg') \
 		or file.endswith('.jpg') or file.endswith('.JPG'):
-			if file != 'filter.png':
+			if 'filter' not in file:
 				im = Image.open(foldername + '/' + file)
-				width, height = im.size
+				print 'Adding', filter_path, 'to ' + file + '...'
 
-				if width > height:
-					height = int((SIZE*1.0/width) * height)
-					width = SIZE
-				else: 
-					width = int((SIZE*1.0/height) * width)
-					height = SIZE
+				# resize fb filter to image size
+				fb_filter = fb_filter.resize(im.size, Image.ANTIALIAS)
 
-				im = im.resize((width, height), Image.ANTIALIAS)
-				
+				# save regular filter on photo
 				im.paste(fb_filter, (0, 0), fb_filter)
-				print 'Adding filter to ' + file + '...'
 				im.save(os.path.join(foldername + '/out', file))
+
+				# black and white
+				im = im.convert('L').convert('RGBA')
+				# add color filter
+				im.paste(fb_filter, (0, 0), fb_filter)
+				im.save(os.path.join(foldername + '/out', 'bw_' + file))
 
 
 if __name__ == '__main__':
